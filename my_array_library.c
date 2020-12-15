72adc9ff47f4651b175a6c46b33148e8b683b37f
Agah <47952065+AgahOz@users.noreply.github.com>
2020-12-15 18:03:36 +0300

    Update my_array_library.c

--- my_array_library.c
+++ my_array_library.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include "my_array_library.h"
 
-
 void print_All_Array (int arr[100][100],int row,int column){
 	int i,j;
 	for(i=0;i<row;i++){
@@ -12,8 +11,8 @@ void print_All_Array (int arr[100][100],int row,int column){
 	}
 	printf("\n");
 	printf("\n");
-
 }
+
 
 void input_Array(int arr[100][100],int row,int column){
 	int i,j;
@@ -34,8 +33,7 @@ void print_nth_mth(int arr[100][100],int row,int column){
 	printf("x: ");	scanf("%d",&N);
 	printf("y: ");	scanf("%d",&M);
 	
-	if((row < N ) || (column < M)){
-	
+	if((N > row-1) || (M > column-1)){
 		printf("Matris aradiginiz boyutlarda degil, dolayisiyla boyle bir deger yok");
 	}
 	else{
@@ -43,8 +41,8 @@ void print_nth_mth(int arr[100][100],int row,int column){
 	}
 	printf("\n");
 	printf("\n");
-
 }
+
 
 void print_Index_Of_Element(int arr[100][100],int row,int column){
 	int key,kontrol = 0;int indexN = 0,indexM=0,i,j;
@@ -103,7 +101,6 @@ void finder_Min_Element(int arr[100][100],int row,int column){
 		}
 	}
 	printf("Dizinin en kucuk elemani %d dir.",min);
-
 	printf("\n");
 	printf("\n");	
 }
@@ -112,24 +109,14 @@ void finder_Max_Element_Rows_Columns(int arr[100][100],int row,int column){
 	int i,j,l,k,m; int maxRow=-999999; int maxColumn = -999999;
 	l = 1;
 	for(i=0;i<row;i++){
-		for(j=0;j<row-1;j++){
+		for(j=0;j<row;j++){
 			if(arr[i][j+1] > maxRow){
 				maxRow = arr[i][j+1];
 			}
-	//******************************************************************************************************************************
-	
-			printf("%d --- ",arr[j+1][i]);
-			printf("%d --->  ",arr[j][i]);
-			printf("%d\n",maxColumn);
-			if(arr[j+1][i] > maxColumn){
-				maxColumn = arr[j+1][i];
+			if(arr[j][i] > maxColumn){
+				maxColumn = arr[j][i];
 			}
-			printf("%d --- ",arr[j+1][i]);
-			printf("%d --->  ",arr[j][i]);
-			printf("%d\n",maxColumn);
 		}
-
-		
 		printf("%d. satirin en buyuk elemani %d dir\n",i+1,maxRow);
 
 		printf("%d. sutunun en buyuk eleamani %d dir\n",l,maxColumn);
@@ -170,26 +157,37 @@ void finder_Transpose_Of_Array(int arr[100][100],int row,int column){
 		}
 	}
 	printf("Yukarida girilen arrayin devrigi asagidadir.\n");
-	print_All_Array(arr2,row,column);
+	print_All_Array(arr2,column,row);
 }
 
-void finder_Product_Of_Two_Arrays(int arr[100][100],int row,int column,int arr2[100][100],int row2,int column2){
-	int i,j; int arrFinal[100][100] = {{11,22},{33,44}};
-	
-	
-	//******************************************************************************************************************************
+int finder_Product_Of_Two_Arrays(int arr[100][100],int row,int column,int arr2[100][100],int row2,int column2){
+	int i,j,k; int arrProduct[100][100];
 
-	
-	print_All_Array(arrFinal,row,column);
+	printf("1. Matris >> \n");
+	print_All_Array(arr,row,column);	
 
-	
+	printf("2. Matris >> \n");
+	print_All_Array(arr2,row2,column2);	
+
+	if(column != row2){
+		printf("Matris carpim kurallarina uymuyor.\n");
+		return 0;
+	}
+	for(i=0;i<row;i++){
+		for(j=0;j<column;j++){
+			for(k=0;k<column;k++){
+				arrProduct[i][j]+= arr[i][k]*arr2[k][j];
+			}
+		}
+	}
+	printf("Iki matrisin carpimi >> \n");
+	print_All_Array(arrProduct,row,column);	
 }
 
 void controller_Whether_Identity_Matrix_Not(int arr[100][100],int row,int column){
 	int i,j; int counterDiag = 0; int counterOutDiag = 0;
 
 	if (row == column){
-	
 		for(i=0;i<row;i++){
 			for(j=0;j<column;j++){
 				if ((i == j)){
@@ -212,7 +210,7 @@ void controller_Whether_Identity_Matrix_Not(int arr[100][100],int row,int column
 		}
 	}
 	else{
-		printf("Sadece kare matrisler için birim matris vardir.\n");
+		printf("Sadece kare matrisler iÃ§in birim matris vardir.\n");
 	}
 	printf("\n");
 	printf("\n");
@@ -222,7 +220,6 @@ void controller_Whether_Symmetric_Matrix_Not(int arr[100][100],int row,int colum
 	int i,j; int counter = 0;
 	
 	if (row == column){
-	
 		for(i=0;i<row;i++){
 			for(j=0;j<column;j++){
 				if (i  == j){				
@@ -239,17 +236,13 @@ void controller_Whether_Symmetric_Matrix_Not(int arr[100][100],int row,int colum
 		if(counter == (row*column - row)){
 			printf("Girilen Matris Simetriktir.");
 		}	
-		
 		else if(counter != (row*column - row)){
 			printf("Girilen Matris Simetrik degildir.");
 		}	
-
 	}
 	else{
-	
 		printf("Sadece kare matrislerin simetrigi vardir.");
 	}
 	printf("\n");
 	printf("\n");
-		
 }
