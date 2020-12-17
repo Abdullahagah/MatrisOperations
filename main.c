d6bfe7d167b39c6c4b1e9ec176461923417cd8f7
Agah <47952065+AgahOz@users.noreply.github.com>
2020-12-17 16:36:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 int main(int argc, char *argv[]) {
 	int row, column;
-	int array[100][100] = {{2,4,6},{8,16,12},{14,26,18}}; row = 3; column = 3;					// sistemde girili temel matrisler
+	int array[100][100] = {{4,5,6},{7,8,4},{2,2,2}}; row = 3; column = 3;					// sistemde girili temel matrisler
 	int testArray[100][100] = {{1,3,5},{7,9,11},{13,15,17}}; int row2 = 3; int column2 = 3; 	// product icin
 	int operation=1; int newMatrisRequest=1; int newSecondMatrisRequest = 0;
 	
 	while(newMatrisRequest){
-	printf("Kendi Matrisinizle islem yapmak icin 1, kayitli matrisle islem yapmak icin 0, cikmak icin -1 giriniz:  ");
+	printf("Kendi Matrisinizle islem yapmak icin 1, kayitli matrisle islem yapmak icin 0, cikmak icin -1 giriniz:  ");	// matris secimi
 	scanf("%d",&newMatrisRequest);
-	if((newMatrisRequest == 0) || (newMatrisRequest== 1) ){
+	if((newMatrisRequest == 0) || (newMatrisRequest== 1) ){  // matris secim kontrolleri  yeni-eski
 		break;
 	}
 	else if (newMatrisRequest != -1) {
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]) {
 
 	if (newMatrisRequest == 1){
 		printf("Matrisin satir boyutunu giriniz: ");scanf("%d",&row);		
-		printf("Matrisin sutun boyutunu giriniz:");scanf("%d",&column);
+		printf("Matrisin sutun boyutunu giriniz: ");scanf("%d",&column);
 		input_Array(array,row,column);
 	}
 	
@@ -33,9 +33,9 @@ int main(int argc, char *argv[]) {
 		printf("\n");
 	}
 	
-	while(operation){
+	while(operation){		// döngü sağlanarak program sürekliliği
 
-		printf("************************************************************************\n");
+		printf("************************************************************************\n");		// işlem sorusu
 		printf("	0- Cikmak icin\n");
 		printf("	1- Matrisi Bastirmak icin\n");
 		printf("	2- Yeni Matris Girmek icin\n");
@@ -53,44 +53,44 @@ int main(int argc, char *argv[]) {
 		printf("\n");	
 
 		printf("\n");
-		printf("Lutfen Yapmak Istediginiz Islemin Numarasini Giriniz: ");
+		printf("Lutfen Yapmak Istediginiz Islemin Numarasini Giriniz >>  ");
 		scanf("%d",&operation);	
 
 		system("CLS");
 		printf("\n");
 
-		switch(operation){
+		switch(operation){		// seçilecek çağrılacak fonksiyon seçimi
 		case(0):
 		printf("\nProgram Sonlandirildi!\n");
 		break;	
 		case(1):
-			print_All_Array(array,row,column);
+			print_All_Array(array,row,column);  // matris bastırma
 			break; 
 		case(2):
-		printf("Matrisin satir boyutunu giriniz: ");scanf("%d",&row);		
+		printf("Matrisin satir boyutunu giriniz: ");scanf("%d",&row);		// matris boyutu alma
 		printf("Matrisin sutun boyutunu giriniz:");scanf("%d",&column);
-		input_Array(array,row,column);
+		input_Array(array,row,column); // matris alma
 			break;
 		case(3):	
-			print_nth_mth(array,row,column);
+			print_nth_mth(array,row,column); // koordinatlara göre matris alma
 			break;
 		case(4):
-			print_Index_Of_Element(array,row,column);
+			print_Index_Of_Element(array,row,column);		// elemanın konumu
 			break;
 		case(5):
-			finder_Number_Of_Nonrepeat_Values(array,row,column);
+			finder_Number_Of_Nonrepeat_Values(array,row,column);	// tekrar etmeyen eleman sayısı
 			break;
 		case(6):
-			finder_Min_Element(array,row,column);
+			finder_Min_Element(array,row,column);	// matrisin en küçük elemanıa
 			break;
 		case(7):
-			finder_Max_Element_Rows_Columns(array,row,column);
+			finder_Max_Element_Rows_Columns(array,row,column);	// her satır ve sütünün en büyük elemanını bastırma
 			break;
 		case(8):
-			sorter_Rows_Decending_By_BubbleSort(array,row,column);
+			sorter_Rows_Decending_By_BubbleSort(array,row,column);  // satırları büyükten küçüğe sıralama
 			break;
 		case(9):
-			finder_Transpose_Of_Array(array,row,column); 
+			finder_Transpose_Of_Array(array,row,column); // matrisin devriğini alma
 			break;
 		case(10):
 			printf("2. Matris icin sistemde girili olan yerine kendi matrisinizi girecekseniz 1, istemiyorsaniz 0 yaziniz: ");
@@ -100,13 +100,13 @@ int main(int argc, char *argv[]) {
 				printf("Matrisin sutun boyutunu giriniz:");scanf("%d",&column2);
 				input_Array(testArray,row2,column2);
 			}
-			finder_Product_Of_Two_Arrays(array,row,column,testArray,row2,column2);
+			finder_Product_Of_Two_Arrays(array,row,column,testArray,row2,column2);  // matris çarpımı
 			break;
 		case(11):
-			controller_Whether_Identity_Matrix_Not(array,row,column);
+			controller_Whether_Identity_Matrix_Not(array,row,column);	// birim matris kontrolü
 			break;
 		case(12):			
-			controller_Whether_Symmetric_Matrix_Not(array,row,column);
+			controller_Whether_Symmetric_Matrix_Not(array,row,column);	// simetrik matris kontrolü
 			break;
 		default:
 			printf("Hatali Islem Kodu, Tekrar deneyin!!\n");
